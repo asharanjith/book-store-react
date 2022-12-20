b1ead3a38377f594be0e9ef47e3d5f2755b38c4c
asharanjith <ashasvijayan@gmail.com>
2022-12-20 19:40:22 +0400

    Connect Redux to React components

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { checksStatus } from '../redux/categories/categories';
 
-const Categories = () => (
-  <div>
-    <h1>Categories</h1>
-    <button type="button">Check Status</button>
-  </div>
-);
+const Categories = () => {
+  const categories = useSelector((state) => state.categories);
+  const dispatch = useDispatch();
+  const clickHandler = () => {
+    dispatch(checksStatus());
+  };
+  return (
+    <div>
+      <h2>{categories}</h2>
+      <button type="submit" onClick={clickHandler}>
+        Check Status
+      </button>
+    </div>
+  );
+};
 
 export default Categories;
--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,22 +1,25 @@
 const ADD_BOOK = 'books/books/ADD_BOOK';
 const REMOVE_BOOK = 'books/books/REMOVE_BOOK';
 
-export const addBook = (book) => ({
-  type: ADD_BOOK,
-  payload: book,
-});
+export const addBook = (data) => (dispatch) => {
+  dispatch({ type: ADD_BOOK, payload: data });
+};
 
-export const removeBook = (state) => ({
-  type: REMOVE_BOOK,
-  books: state.books.filter((book) => book.id !== state.book.id),
-});
+export const removeBook = (id) => (dispatch) => {
+  dispatch({ type: REMOVE_BOOK, payload: id });
+};
 
-const booksReducer = (state = [], action) => {
+const books = [
+  { title: 'The Hunger Games', author: 'Suzanne', id: 1 },
+  { title: 'Dune', author: 'Frank Herbert', id: 2 },
+];
+
+const booksReducer = (state = books, action) => {
   switch (action.type) {
     case ADD_BOOK:
       return [...state, action.payload];
     case REMOVE_BOOK:
-      return [...state, action.books];
+      return state.filter((book) => book.id !== action.payload);
     default:
       return state;
   }
